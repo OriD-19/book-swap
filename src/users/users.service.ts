d323bb01b197599c338e5f6596818fa14b76f0a9
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-06-13 13:22:23 -0600

    Configured everything user related

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,7 +13,7 @@ export class UsersService {
     ) { }
 
     create(createUserDto: CreateUserDto) {
-        return 'This action adds a new user';
+        return this.userRepository.save(createUserDto);
     }
 
     findAll() {
